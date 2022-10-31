595102b4f48d6af7159853cfc51bfc38cf96e1a2
Raven Munonela <ravenmunonela@gmail.com>
2022-10-31 20:24:21 +0200

    finished cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -6,15 +6,23 @@ import {useDispatch, useSelector} from 'react-redux'
 //ICONS
 import {TiArrowSortedDown, TiArrowSortedUp} from 'react-icons/ti'
 import { ImCross } from "react-icons/im";
+import { setIsCartOpenFalse, increaseAmount, decreaseAmount, setCartTotal, removeItem, setClearCart } from '../features/products/cart-slice';
+import { useEffect } from 'react';
 
 function Cart() {
     const dispatch = useDispatch();
-    const {cartItems} = useSelector((store) => store.cartSlice)
+    const {cartItems, cartTotal} = useSelector((store) => store.cartSlice)
+
+    useEffect(() => {
+      dispatch(setCartTotal())
+    }, [cartItems])
 
   return <section className='cart-container'>
     <div className='cart-heading-container'>
-      <h1>Your Cart Awaiteth</h1>
-      <ImCross/>
+      {cartItems.length === 0 ? <h2>Your Shopping Cart Is Empty</h2> : <h1>Shopping Cart</h1>}
+      <button className='cart-cross-icon-btn' onClick={() => dispatch(setIsCartOpenFalse())}>
+      <ImCross className='cart-cross-icon'/>
+      </button>
     </div>
     {
       cartItems.map((item) => {
@@ -22,25 +30,39 @@ function Cart() {
 
         return <div className='cart-item-container' key={id}>
           <img src={image} alt={title} id='cart-item-image'/>
-          <div>
-            <h3>{title}</h3>
-            <h4>${price}</h4>
-            <button>
+          <div className='cart-middle-info-container'>
+            <h4>{title}</h4>
+            <h5>${price}</h5>
+            <button onClick={() => dispatch(removeItem({id, amount}))}>
               remove item
             </button>
           </div>
           <div className='cart-item-btn-container'>
-            <button>
+            <button onClick={() => dispatch(increaseAmount(id))}>
               <TiArrowSortedUp className='increase-decrease-icons'/>
             </button>
               {amount}
-            <button>
+            <button onClick={() => dispatch(decreaseAmount(id))}>
               <TiArrowSortedDown className='increase-decrease-icons'/>
             </button>
           </div>
         </div>
       })
     }
+    <div className='cart-footer-container'>
+      <div className="cart-total-container">
+        <h3>Total</h3>
+        <h3>$ {cartTotal}</h3>
+      </div>
+      <div className='cart-checkout-container'>
+        <button onClick={() => dispatch(setClearCart())}>
+          Clear Cart
+        </button>
+        <button>
+          Check Out
+        </button>
+      </div>
+    </div>
   </section>
 }
 
--- src/features/products/cart-slice.js
+++ src/features/products/cart-slice.js
@@ -3,9 +3,8 @@ import { createSlice} from "@reduxjs/toolkit";
 const initialState = {
     cartItems: [],
     isCartOpen: false,
-    allItemTotal: 0,
-    individualItemSum: [0, ''],
-    // individualTotalNumber: 0
+    cartTotal: 0,
+    totalCartItems: 0,
 }
 
 
@@ -15,16 +14,77 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const data = action.payload;
+            const [data] = action.payload;
+            const {id} = data;
 
-            state.cartItems = data; 
+            if (state.cartItems.length === 0) {
+                state.cartItems = [data];
+
+                
+            } else {
+                if (id) {
+                    const foundItem = state.cartItems.find((item) => item.id === id);
+                    if(foundItem) {
+                        foundItem.amount += 1;
+                    } else {
+                        state.cartItems.push(data);
+                    }
+
+                }
+            }
+            
+            state.totalCartItems = state.totalCartItems + 1;
+        },
+        removeItem: (state, action) => {
+            const {id, amount} = action.payload;
+
+            state.cartItems = state.cartItems.filter((item) => item.id !== id);
+            state.totalCartItems = state.totalCartItems - amount;
+        },
+        increaseAmount: (state, action) => {
+            const id = action.payload
+            const foundItem = state.cartItems.find((item) => item.id === id);
+            foundItem.amount += 1;
+            
+            //Increase number of items in cart
+            state.totalCartItems = state.totalCartItems + 1;
+        },
+        decreaseAmount: (state, action) => {
+            const id = action.payload
+            const foundItem = state.cartItems.find((item) => item.id === id);
+            if (foundItem.amount === 1) {
+                return;
+            } else {
+                foundItem.amount -= 1;
+
+                //decrease number of items in cart
+                state.totalCartItems = state.totalCartItems - 1;
+            }
+            
         },
         setIsCartOpenTrue: (state) => {
             state.isCartOpen = true;
+            
+        },
+        setIsCartOpenFalse: (state) => {
+            state.isCartOpen = false;
+        },
+        setCartTotal: (state) => {
+            //Mathematics
+            let total = 0;
+
+            state.cartItems.forEach((item) => {
+                total += item.price * item.amount;
+            })
+
+            state.cartTotal = total.toFixed(2);
+        },
+        setClearCart: (state) => {
+            state.cartItems = [];
         }
     }
 })
 
 
-export const {addToCart, setIsCartOpenTrue} = cartSlice.actions;
+export const {addToCart, removeItem, setIsCartOpenTrue, setIsCartOpenFalse,decreaseAmount, increaseAmount, setCartTotal, setClearCart} = cartSlice.actions;
 export default cartSlice.reducer;
