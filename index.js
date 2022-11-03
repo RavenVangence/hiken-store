e63ee4571aaa0ae4af2933bdf2f8e3cd3b09d8d8
Raven Munonela <ravenmunonela@gmail.com>
2022-11-03 12:59:24 +0200

    server first commit-5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 8000 ;
 const cors = require('cors');
 // R O U T E S
 const homeRoutes = require('../server/routes/home/products-route')
@@ -12,9 +12,8 @@ app.use(cors());
 app.use('/', homeRoutes);
 app.use('/cart/item', cartRoutes);
 
-
-app.listen(port, console.log('listening on port 8000'));
-
 app.get('/', (req, res) => {
     res.send('test successful');
 });
+
+app.listen(port, console.log('listening on port 8000'));
